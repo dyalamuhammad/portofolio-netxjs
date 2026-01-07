f84d4a852bc6979b10c6a65a01de4693fc580e00
dyalamuhammad <dyalamuhammad@gmail.com>
2026-01-08 00:01:24 +0700

    adjust jakarta

--- src/app/components/home/about-me/index.tsx
+++ src/app/components/home/about-me/index.tsx
@@ -20,7 +20,7 @@ const AboutMe = () => {
                     <div className="flex flex-col gap-9 sm:gap-12 max-w-4xl mx-auto px-4 sm:px-7 py-11 md:py-20">
                         <div className="flex flex-col gap-4">
                             <p className="text-sm tracking-[2px] text-primary uppercase font-medium">About Me</p>
-                            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[32px]">Hi, I’m Dyala — a Full Stack Web Developer based in Indonesia, focused on <span className="bg-[linear-gradient(90deg,_rgba(243,202,77,0.4)_0%,_rgba(243,202,77,0.05)_100%)]">building scalable and user-focused web applications</span> using modern technologies.</h2>
+                            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[32px]">Hi, I’m Dyala — a Full Stack Web Developer based in Jakarta, focused on <span className="bg-[linear-gradient(90deg,_rgba(243,202,77,0.4)_0%,_rgba(243,202,77,0.05)_100%)]">building scalable and user-focused web applications</span> using modern technologies.</h2>
                             <h5 className="text-secondary font-normal">Experienced in Laravel, Next.js, Express.js, and PostgreSQL, with a strong background in frontend performance, backend development, and collaborative product delivery.</h5>
                         </div>
                         <div className="flex flex-col gap-4">
